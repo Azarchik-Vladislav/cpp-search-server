313de9be07ad88d1dcda0e5c15814c644c13f2f3
Azarchik Vladislav <vlad.azarchik@yandex.ru>
2024-03-17 23:54:18 +0500

    Добавлена фукция разделитель для тестовых функций

--- search-server/test_programm_functions.cpp
+++ search-server/test_programm_functions.cpp
@@ -1,12 +1,16 @@
 
 #include "test_programm_functions.h"
 
+void OutputParseLine() {
+    for(int i = 25; i > 0; --i){
+        std::cerr << "*";
+    }
+    std::cerr<< std::endl;
+}
+
 
 void CheckExeptionOutput() {
-    for(int i = 25; i > 0; --i){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
+    
     TestExeptionForConstructorWithStopWords();
     TestExeptionForDocumentsWithSameIndex();
     TestExeptionForDocumentsWithNegativeIndex();
@@ -14,17 +18,11 @@ void CheckExeptionOutput() {
     TestExeptionForQueryWithSpecialCharacters();
     TestExeptionForQueryWithMinusWords();
     TestExeptionForGetDocumentId();
-    for(int i = 25; i > 0; --i){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
+    
 }
 
 void TestSearchServer() {
-    for(int i = 25; i > 0; --i){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
+    OutputParseLine();
     RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
     RUN_TEST(TestExcludeDocumentsIfMinusWords);
     RUN_TEST(TestSortAndCalculateByRelevance);
@@ -32,39 +30,24 @@ void TestSearchServer() {
     RUN_TEST(TestOutDocumentsWithPredicate);
     RUN_TEST(TestOutDicumentWithQueryStatus);
     RUN_TEST(TestReturnStatusFromMatchDocument);
-    for(int i = 25; i > 0; --i){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
+    OutputParseLine();
 }
 
 void TestPaginator() {
-    for(int i = 25; i > 0; --i){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
+    OutputParseLine();
 
     RUN_TEST(TestCountOfPages);
     RUN_TEST(TestEndPage);
 
-    for(int i = 25; i > 0; --i){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
+    OutputParseLine();
 }
 
 void TestRequestQueue() {
-    for(int i = 25; i > 0; --i){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
+    OutputParseLine();
 
     RUN_TEST(TestFullQueueQueries);
     RUN_TEST(TestNoFullQueue);
 
-    for(int i = 25; i > 0; --i){
-        std::cout << "*";
-    }
-    std::cout << std::endl;
+    OutputParseLine();
     
 }
